94d851bb48e4bdb99c5f51eea47dbd8875964625
roger21 <roger21@free.fr>
2026-04-07 00:15:39 +0200

    hop

--- hfr_pas_d_alerte_light.user.js
+++ hfr_pas_d_alerte_light.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          [HFR] Pas d'alerte light
-// @version       1.0.6
+// @version       1.0.7
 // @namespace     roger21.free.fr
 // @description   Permet de savoir - de manière légère et rapide - si une alerte de modération a été lancée en passant la souris sur le bouton.
 // @icon          data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAilBMVEX%2F%2F%2F8AAADxjxvylSrzmzf5wYLzmjb%2F9er%2F%2Fv70nj32q1b5woT70qT82rT827b%2F%2B%2FjxkSHykybykyfylCjylCnzmDDzmjX0nTv1o0b1qFH2qVL2qlT3tGn4tmz4uHD4uXL5vHf83Lf83Lj937394MH%2B587%2B69f%2F8%2BX%2F8%2Bf%2F9On%2F9uz%2F%2BPH%2F%2BvT%2F%2FPmRE1AgAAAAwElEQVR42s1SyRbCIAysA7W2tdZ93%2Ff1%2F39PEtqDEt6rXnQOEMhAMkmC4E9QY9j9da1OkP%2BtTiBo1caOjGisDLRDANCk%2FVIHwwkBZGReh9avnGj2%2FWFg%2Feg5hD1bLZTwqdgU%2FlTSdrqZJWN%2FKImPOnGjiBJKhYqMvikxtlhLNTuz%2FgkxjmJRRza5mbcXpbz4zldLJ0lVEBY5nRL4CJx%2FMEfXE4L9j4Qr%2BZakpiandMpX6FO7%2FaPxxUTJI%2FsJ4cd4AoSOBgZnPvgtAAAAAElFTkSuQmCC
@@ -17,7 +17,7 @@
 
 /*
 
-Copyright © 2018-2020 roger21@free.fr
+Copyright © 2018-2020, 2026 roger21@free.fr
 
 This program is free software: you can redistribute it and/or modify it under the
 terms of the GNU Affero General Public License as published by the Free Software
@@ -32,9 +32,11 @@ with this program. If not, see <https://www.gnu.org/licenses/agpl.txt>.
 
 */
 
-// $Rev: 3436 $
+// $Rev: 4676 $
 
 // historique :
+// 1.0.7 (06/04/2026) :
+// - bidouille pour éviter que le bouton d'alerte ait une taille nulle (bug du script)
 // 1.0.6 (13/02/2020) :
 // - utilisation d'une url en data pour l'icône du script et changement d'hébergeur (free.fr -> github.com)
 // 1.0.5 (11/02/2020) :
@@ -142,6 +144,12 @@ for(let button of buttons) {
   button.addEventListener("mouseover", mouseover, false);
   button.addEventListener("mouseout", mouseout, false);
   button.dataset.src = button.src;
-  button.style.width = button.naturalWidth + "px";
-  button.style.height = button.naturalHeight + "px";
+  // taille fixe du bouton pour éviter le redimensionnement lors de l’affichage du throbber
+  // en mode shadow uniquement, en mode icons on laisse les images avoir leur taille réelle
+  if(!icons) {
+    window.setTimeout(function() {
+      button.style.width = button.naturalWidth + "px";
+      button.style.height = button.naturalHeight + "px";
+    }, 200);
+  }
 }
